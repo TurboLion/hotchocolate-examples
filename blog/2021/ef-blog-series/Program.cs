444909ed8b63a6eeddab6792f82d47758e1a3759
TurboLion <TurboLion@users.noreply.github.com>
2021-10-12 10:15:52 +0300

    Add Diagnostic

--- blog/2021/ef-blog-series/Program.cs
+++ blog/2021/ef-blog-series/Program.cs
@@ -14,7 +14,9 @@ builder.Services
     .AddFiltering()
     .AddSorting()
     .EnsureDataIsSeeded()
-    .InitializeOnStartup();
+    .InitializeOnStartup()
+    .SetPagingOptions(new HotChocolate.Types.Pagination.PagingOptions { IncludeTotalCount = true }) // Added
+    .AddDiagnosticEventListener<MyExecutionEventListener>(); // Added
 
 var app = builder.Build();
 
@@ -22,3 +24,40 @@ app.MapGraphQL();
 
 app.Run();
 
+// Added:
+public class MyExecutionEventListener : HotChocolate.Execution.Instrumentation.ExecutionDiagnosticEventListener
+{
+    private readonly ILogger<MyExecutionEventListener> _logger;
+
+    public MyExecutionEventListener(ILogger<MyExecutionEventListener> logger)
+        => _logger = logger;
+
+    // this is invoked at the start of the `ExecuteRequest` operation
+    public override IDisposable ExecuteRequest(IRequestContext context)
+    {
+        var start = DateTime.UtcNow;
+
+        return new RequestScope(start, _logger);
+    }
+}
+
+public class RequestScope : IDisposable
+{
+    private readonly ILogger _logger;
+    private readonly DateTime _start;
+
+    public RequestScope(DateTime start, ILogger logger)
+    {
+        _start = start;
+        _logger = logger;
+    }
+
+    // this is invoked at the end of the `ExecuteRequest` operation
+    public void Dispose()
+    {
+        var end = DateTime.UtcNow;
+        var elapsed = end - _start;
+
+        _logger.LogInformation("Request finished after {Ticks} ticks", elapsed.Ticks);
+    }
+}
